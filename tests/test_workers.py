3bc1c85d8ceca672f1e4531e624601030178c719
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-03-30 14:28:42 +0100

    and this

--- tests/test_workers.py
+++ tests/test_workers.py
@@ -126,7 +126,7 @@ def test_gunicorn_arbiter_signal_handling(gunicorn_process: Process, signal_to_s
     time.sleep(0.5)
     output_text = gunicorn_process.read_output()
     try:
-        assert expected_text in output_text
+        assert expected_text in output_text, output_text
     except AssertionError:  # pragma: no cover
         # occasional flakes are seen with certain signals
         flaky_signals = [
@@ -140,7 +140,7 @@ def test_gunicorn_arbiter_signal_handling(gunicorn_process: Process, signal_to_s
         if signal_to_send not in flaky_signals:
             time.sleep(2)
             output_text = gunicorn_process.read_output()
-            assert expected_text in output_text, signal_to_send
+            assert expected_text in output_text
 
 
 async def app_with_lifespan_startup_failure(scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable) -> None:
